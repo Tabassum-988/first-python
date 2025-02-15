edbbfb1941626b95fcf4ff4796104ef87d4fe964
Tabassum-988 <sultanatabassum837@gmail.com>
2025-02-15 22:28:58 +0600

    first python

---
+++ type.py
@@ -0,0 +1,29 @@
+#Numeric deta
+#int
+X=10
+print(X)
+#float
+Y=12.7
+print(Y)
+#complex
+Z=10+20j
+#String
+Letter=["A","B","C"]
+print(Letter)
+Country=("Canada","Japan","Finland")
+print(Country)
+#Sequence(List(mutable)-Just like array)
+list=[10,20.5,"Tabassum","Japan"]
+print(list[2])
+#Tuple(JUst like list but immutable)
+id=("234","543","228")
+print(id)
+#Range
+number=range(1,10)
+print(*number)
+odd_number=range(1,10,2)
+print(*odd_number)
+even_number=range(2,20,2)
+print(*even_number)
+num=range(5)
+print(*num)
