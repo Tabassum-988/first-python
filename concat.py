0908a24cc5907d7e671f082aefaacf4cb5770f87
Tabassum-988 <sultanatabassum837@gmail.com>
2025-02-24 21:26:21 +0600

    first python

---
+++ concat.py
@@ -0,0 +1,18 @@
+#string repetition
+text="Hello World "
+repeat=text * 3 #String * number of repetition
+print(repeat)
+
+#string concatenation
+#using (+) operator
+str1="Tabassum"
+str2="Sultana"
+concat1=("My first name "+str1 +"\nAnd My last name "+str2)
+print(concat1)
+#using (%)
+string1="Shahriar"
+string2="Spondon"
+combine2="%s %s" %(string1,string2)
+print(combine2)
+#otherwise using join or format method
+
